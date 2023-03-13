02a22317d3fa89c4e6e84bd1e20567ef310ab11c
Standard Computer's <Standard Computer's@DESKTOP-FRC77TS>
2023-03-13 15:56:49 +0300

    Add project files.

---
+++ WPF_Password/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Bogus;
+
+namespace WPF_Password
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        private void Passwod_Click(object sender, RoutedEventArgs e)
+        {
+            SendTextBox.Visibility = Visibility.Visible;
+            SendTextBox.Focus();
+
+            btnSend.Visibility = Visibility.Visible;
+            btnSend.Focus();
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            PasswodTextBox.Text = SendTextBox.Text;
+        }
+
+        private void AutoPasswod_Click(object sender, RoutedEventArgs e)
+        {
+            var faker = new Faker();
+            string password = faker.Internet.Password();
+            PasswodTextBox.Text = password;
+        }
+    }
+}
